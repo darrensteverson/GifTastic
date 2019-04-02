52d9208a605d448f898991ff12e09da00da707f2
darren steverson <dcs194@scarletmail.rutgers.edu>
2019-04-02 13:33:09 -0400

    dynamically created, text field to generate new button

--- logic.js
+++ logic.js
@@ -1,13 +1,37 @@
 var workouts = ["Squats", "Curls", "Pullups", "Lunges"];
 
- // Adding click event listen listener to all buttons
- $("add-workout").on("click", function() {
-    // Grabbing and storing the data-animal property value from the button
-    var workout = $(this).attr("data-workout");
+ function createButton (){
+// No repeat Buttons
+  $("#buttons-view").empty();
 
-    // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      workout + "&api_key=rAAIhnZOVdDGfx5e7XgcvmVohy8R723d";
+  for (var i = 0; i < workouts.length; i++) {
+      // Dynamically create Button
+      var generateButton = $("<button>");
+      // add class
+      generateButton.addClass ("workout");
+      // add attribute
+      generateButton.attr("data-workout", workouts[i]);
+      // button text
+      generateButton.text(workouts[i]);
+      // Add button to HTML
+      $("#buttons-view").append(generateButton)
+
+  }
+}
+
+// When button is clicked
+$("#add-workout").on("click", function (event) {
+  event.preventDefault();
+
+  workout = $("#workout-input").val().trim();
+// adding workout to workouts array
+  workouts.push(workout);
+  createButton();
+
+});
 
 
- });
+
+
+
+ 
