407e3a7cf419e021649346ac143169e5dfea8f9d
darren steverson <dcs194@scarletmail.rutgers.edu>
2019-04-04 12:14:35 -0400

    added in event listener for dynamically created button on document. Can't get the Gifs to appear after clicking one of the dynamivaly created buttons.

--- logic.js
+++ logic.js
@@ -1,20 +1,20 @@
-var workouts = ["Squats", "Curls", "Pullups", "Lunges"];
+var workouts = ["Squats", "Pullups", "Lunges"];
 
- function createButton (){
-// No repeat Buttons
+function createButton() {
+  // No repeat Buttons
   $("#buttons-view").empty();
 
   for (var i = 0; i < workouts.length; i++) {
-      // Dynamically create Button
-      var generateButton = $("<button>");
-      // add class
-      generateButton.addClass ("workout");
-      // add attribute
-      generateButton.attr("data-workout", workouts[i]);
-      // button text
-      generateButton.text(workouts[i]);
-      // Add button to HTML
-      $("#buttons-view").append(generateButton)
+    // Dynamically create Button
+    var generateButton = $("<button>");
+    // add class
+    generateButton.addClass("workout");
+    // add attribute
+    generateButton.attr("data-workout", workouts[i]);
+    // button text
+    generateButton.text(workouts[i]);
+    // Add button to HTML
+    $("#buttons-view").append(generateButton);
 
   }
 }
@@ -23,15 +23,51 @@ var workouts = ["Squats", "Curls", "Pullups", "Lunges"];
 $("#add-workout").on("click", function (event) {
   event.preventDefault();
 
-  workout = $("#workout-input").val().trim();
-// adding workout to workouts array
-  workouts.push(workout);
+  var workoutType = $("#workout-input").val().trim();
+  // adding workout to workouts array
+  workouts.push(workoutType);
   createButton();
 
 });
+$(document).on("click", ".workout");
+createButton();
+
+
+
+
+// Adding event listener to all buttons
+$(document).on("click", ".workout", function () {
+
+  // queryURL with workout in the search
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=workouts&api_key=rAAIhnZOVdDGfx5e7XgcvmVohy8R723d&limit=5";
+
+  // Ajax request
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  // data from the AJAX request comes back
+    .then(function (response) {
+
+      // Saving the image_original_url property
+      var imageUrl = response.data.image_original_url;
+
+// Creating and storing an image tag
+      var workoutImage = $("<img>");
+
+       // Setting the catImage src attribute to imageUrl
+      workoutImage.attr("src", imageUrl);
+      workoutImage.attr("alt", "workout image");
+
+      // prepending img to html gif-appear-here div
+      $("#gifs-appear-here").prepend(workoutImage);
+
+    });
+  });
 
 
 
 
 
- 
+
+
